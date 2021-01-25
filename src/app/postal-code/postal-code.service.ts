29b36fbe67989c3080d6bc54edc472660964b92d
Mike <1334914120@qq.com>
2021-01-25 13:16:04 +0800

    clear unused declaration

--- src/app/postal-code/postal-code.service.ts
+++ src/app/postal-code/postal-code.service.ts
@@ -1,5 +1,4 @@
 import { HttpClient, HttpParams } from '@angular/common/http'
-import { unescapeIdentifier } from '@angular/compiler'
 import { Injectable } from '@angular/core'
 import { Observable } from 'rxjs'
 import { defaultIfEmpty, mergeMap } from 'rxjs/operators'
